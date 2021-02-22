9f233d3eae72bf3ece099086bb961403d759dad8
sakshi67 <skpgh2007@gmail.com>
2021-02-22 12:56:48 +0530

    created Chain

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +36,10 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    
+
+    log6 = new Log(230,180,80, PI/2);
+    chain = new Chain(bird.body,log6.body);
 
 }
 
@@ -61,4 +66,7 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    log6.display();
+    chain.display();
 }
